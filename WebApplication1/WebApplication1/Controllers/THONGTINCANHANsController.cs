5d577249206be6e2b82150e4165981ef792fa761
197PM09476 - Nguyễn Thành Luân - K25T-PM <luan.197pm09476@vanlanguni.vn>
2021-06-03 15:35:26 +0700

    Chuc nang ThongTinCaNhan

---
+++ WebApplication1/WebApplication1/Controllers/THONGTINCANHANsController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class THONGTINCANHANsController : Controller
+    {
+        private CT25Team15Entities db = new CT25Team15Entities();
+
+        // GET: THONGTINCANHANs
+        public ActionResult Index()
+        {
+
+            return View(db.THONGTINCANHANs.ToList());
+        }
+        public ActionResult Index2(string id)
+        {
+            var user = db.THONGTINCANHANs.Find(id);
+            return View(user);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index2(THONGTINCANHAN tHONGTINCANHAN)
+        {
+            if (ModelState.IsValid)
+            {
+                var updatettcanhan = db.THONGTINCANHANs.Find(tHONGTINCANHAN.id);
+                updatettcanhan.NgaySinh = tHONGTINCANHAN.NgaySinh;
+                updatettcanhan.SDT = tHONGTINCANHAN.SDT;
+                updatettcanhan.Ten = tHONGTINCANHAN.Ten;
+                updatettcanhan.GioiTinh = tHONGTINCANHAN.GioiTinh;
+                var user = db.AspNetUsers.Find(tHONGTINCANHAN.id);
+                user.PhoneNumber = tHONGTINCANHAN.SDT;
+                db.SaveChanges();
+                return RedirectToAction("Index2");
+            }
+            ViewBag.id = new SelectList(db.AspNetUsers, "Id", "Email", tHONGTINCANHAN.id);
+            return View(tHONGTINCANHAN);
+        }
+        // GET: THONGTINCANHANs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            THONGTINCANHAN tHONGTINCANHAN = db.THONGTINCANHANs.Find(id);
+            if (tHONGTINCANHAN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tHONGTINCANHAN);
+        }
+
+        // GET: THONGTINCANHANs/Creat
+
+        // GET: THONGTINCANHANs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            THONGTINCANHAN tHONGTINCANHAN = db.THONGTINCANHANs.Find(id);
+            if (tHONGTINCANHAN == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id = new SelectList(db.AspNetUsers, "Id", "Email", tHONGTINCANHAN.id);
+            return View(tHONGTINCANHAN);
+        }
+
+        // POST: THONGTINCANHANs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(THONGTINCANHAN tHONGTINCANHAN)
+        {
+            if (ModelState.IsValid)
+            {
+                var updatettcanhan = db.THONGTINCANHANs.Find(tHONGTINCANHAN.id);
+                updatettcanhan.NgaySinh = tHONGTINCANHAN.NgaySinh;
+                updatettcanhan.SDT = tHONGTINCANHAN.SDT;
+                updatettcanhan.Ten = tHONGTINCANHAN.Ten;
+                updatettcanhan.GioiTinh = tHONGTINCANHAN.GioiTinh;
+                var user = db.AspNetUsers.Find(tHONGTINCANHAN.id);
+                user.PhoneNumber = tHONGTINCANHAN.SDT;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id = new SelectList(db.AspNetUsers, "Id", "Email", tHONGTINCANHAN.id);
+            return View(tHONGTINCANHAN);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
