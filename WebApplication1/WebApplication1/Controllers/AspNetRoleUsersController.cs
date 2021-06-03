86a3c953803469349aa0cb4b0e3ee37f53e04ec5
197PM09476 - Nguyễn Thành Luân - K25T-PM <luan.197pm09476@vanlanguni.vn>
2021-06-03 15:29:00 +0700

    Add AspNetRoleUsers

---
+++ WebApplication1/WebApplication1/Controllers/AspNetRoleUsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AspNetRoleUserController : Controller
+    {
+        private CT25Team15Entities db = new CT25Team15Entities();
+
+        public ActionResult Create(string roleId)
+        {
+            ViewBag.Role = db.AspNetRoles.Find(roleId);
+            ViewBag.User = new SelectList(db.AspNetUsers, "Id", "UserName");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(string roleId, string userId)
+        {
+            var role = db.AspNetRoles.Find(roleId);
+            var user = db.AspNetUsers.Find(userId);
+
+            role.AspNetUsers.Add(user);
+            db.Entry(role).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "AspNetRoles");
+        }
+
+
+        public ActionResult Delete(string roleId, string userId)
+        {
+            var role = db.AspNetRoles.Find(roleId);
+            var user = db.AspNetUsers.Find(userId);
+
+            role.AspNetUsers.Remove(user);
+            db.Entry(role).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index", "AspNetRoles");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
