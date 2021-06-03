e87a42192e110edb13a810a9cf8c74004c5b73b9
197PM09431 - Lại Khắc Đạt - K25T-PM <dat.197pm09431@vanlanguni.vn>
2021-06-03 18:41:46 +0700

    Login Logout, Login VLU

--- WebApplication1/WebApplication1/App_Start/Startup.Auth.cs
+++ WebApplication1/WebApplication1/App_Start/Startup.Auth.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.VanLang;
 using Owin;
 using WebApplication1.Models;
 
@@ -63,6 +63,7 @@ namespace WebApplication1
             //    ClientId = "",
             //    ClientSecret = ""
             //});
+            app.UseVanLangAuthentication("https://fitlogin.vanlanguni.edu.vn/Login");
         }
     }
 }
