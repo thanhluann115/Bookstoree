14bacc8166f97fd42c2ed5f00d2dc4231defbc09
197PM09476 - Nguyễn Thành Luân - K25T-PM <luan.197pm09476@vanlanguni.vn>
2021-06-03 15:26:40 +0700

    Add AspNetRoles

---
+++ WebApplication1/WebApplication1/Controllers/AspNetRolesController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AspNetRolesController : Controller
+    {
+        private CT25Team15Entities db = new CT25Team15Entities();
+
+        // GET: AspNetRoles
+        public ActionResult Index()
+        {
+            return View(db.AspNetRoles.ToList());
+        }
+
+        // GET: AspNetRoles/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetRole aspNetRole = db.AspNetRoles.Find(id);
+            if (aspNetRole == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetRole);
+        }
+
+        // GET: AspNetRoles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AspNetRoles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AspNetRole aspNetRole)
+        {
+            if (ModelState.IsValid)
+            {
+                aspNetRole.Id = Guid.NewGuid().ToString();
+                db.AspNetRoles.Add(aspNetRole);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aspNetRole);
+        }
+
+        // GET: AspNetRoles/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetRole aspNetRole = db.AspNetRoles.Find(id);
+            if (aspNetRole == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetRole);
+        }
+
+        // POST: AspNetRoles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] AspNetRole aspNetRole)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aspNetRole).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aspNetRole);
+        }
+
+        // GET: AspNetRoles/Delete/5
+
+
+        public ActionResult Delete(string delete)
+        {
+            AspNetRole aspNetRole = db.AspNetRoles.Find(delete);
+            db.AspNetRoles.Remove(aspNetRole);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
