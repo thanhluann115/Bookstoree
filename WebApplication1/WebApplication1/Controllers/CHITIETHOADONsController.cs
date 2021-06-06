d7a254380160e31eafed782828ab779982c9b30f
197PM09476 - Nguyễn Thành Luân - K25T-PM <luan.197pm09476@vanlanguni.vn>
2021-06-06 17:00:14 +0700

    Update 1.2

---
+++ WebApplication1/WebApplication1/Controllers/CHITIETHOADONsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CHITIETHOADONsController : Controller
+    {
+        private CT25Team15Entities db = new CT25Team15Entities();
+
+        // GET: CHITIETHOADONs
+        public ActionResult Details(int id)
+        {
+            var cHITIETHOADONs = db.CHITIETHOADONs.Where(c=>c.MaHD == id).ToList();
+            return View(cHITIETHOADONs);
+        }
+
+       
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
